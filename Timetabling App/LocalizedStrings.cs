3ffad40e7ce7683fa64dea2f7b7dfbb4431968d5
Antagonist121 <psyaww@nottingham.ac.uk>
2015-11-29 01:54:23 +0000

    Initial Commit

---
+++ Timetabling App/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Timetabling_App.Resources;
+
+namespace Timetabling_App
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
