0eca6c2007819100cac3256fb1878a0987c72ccf
Antagonist121 <psyaww@nottingham.ac.uk>
2015-11-29 06:37:16 +0000

    missing file

---
+++ Timetabling App/Services/WeekTimetableRetrieverService.cs
@@ -0,0 +1,12 @@
+﻿using Timetabling_App.Models;
+
+namespace Timetabling_App.Services
+{
+    public class WeekTimetableRetrieverService
+    {
+        public Week getSchedule()
+        {
+            return null;
+        }
+    }
+}
